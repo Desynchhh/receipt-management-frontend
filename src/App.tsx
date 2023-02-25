fa60955bd21440854c59b3390ab7d506864ea02c
Desynch <desynch939@gmail.com>
2023-02-25 17:21:55 +0100

    Init commit v2.0

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className="text-3xl mb-3">Hello world!</h1>
+      <button className="px-5 py-2 border border-solid border-blue-300 text-blue-300 rounded-xl" onClick={() => setCount(prevCount => prevCount+1)}>{count}</button>
+    </div>
+  )
+}
+
+export default App
