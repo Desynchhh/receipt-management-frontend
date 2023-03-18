11d6e0d1c688ca8d565023136e320b19617fca06
Desynch <desynch939@gmail.com>
2023-03-18 20:10:49 +0100

    Refactored ReceiptNew and ReceiptForm. Made up for bad practices.

--- src/components/receipt/ReceiptForm.tsx
+++ src/components/receipt/ReceiptForm.tsx
@@ -4,33 +4,35 @@ import { FormErrors } from "../Form"
 
 interface Props {
   receiptStore: string,
-  setReceiptStore: React.Dispatch<React.SetStateAction<string>>,
+  onSetStore: (e: React.ChangeEvent<HTMLInputElement>) => void,
   dateBought: ReceiptDate,
-  setDateBought: React.Dispatch<React.SetStateAction<ReceiptDate>>,
+  onChangeDate: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  onAddItem: () => void,
+  onSubmit: () => void,
 }
 
-const ReceiptForm = (props:React.PropsWithChildren<Props>) => {
+const ReceiptForm = (props: React.PropsWithChildren<Props>) => {
   const [errors, setErrors] = useState<string[]>([]);
-  
+
   // Found out there is a "date" type on input elements after writing this...
   const validateDateFormat = () => {
     setErrors([])
-    if(props.dateBought.length <= 0) return;
+    if (props.dateBought.length <= 0) return;
 
     const isDate = props.dateBought.match(receiptDateRegex);
     const isDateTime = props.dateBought.match(receiptDateTimeRegex);
-    if(!isDate && !isDateTime) {
+    if (!isDate && !isDateTime) {
       setErrors(["Date is not in a proper format!\n Please use the format YYYY-MM-DD"]);
     }
   }
 
-  return(
+  return (
     <>
       <form>
         <div className="flex flex-col mb-2">
           <div className="flex flex-col mb-1">
             <label className="mr-1" htmlFor="store">Store</label>
-            <input type="text" placeholder="Store" name="store" value={props.receiptStore} onChange={(e) => props.setReceiptStore(e.target.value)} className="text-black mb-2" />
+            <input type="text" placeholder="Store" name="store" value={props.receiptStore} onChange={props.onSetStore} className="text-black mb-2" />
           </div>
           <div className="flex flex-col mb-1">
             <label className="mr-1" htmlFor="date">Date bought</label>
@@ -40,14 +42,14 @@ const ReceiptForm = (props:React.PropsWithChildren<Props>) => {
               placeholder="Date bought"
               name="date"
               value={props.dateBought}
-              onChange={(e) => {
-                props.setDateBought(e.target.value as ReceiptDate);
-              }}
+              onChange={props.onChangeDate}
               onBlur={validateDateFormat}
             />
             {errors.length > 0 && <FormErrors errors={errors} />}
           </div>
         </div>
+        <button className="button" onClick={props.onAddItem}>Add item</button>
+        <input type="submit" name="submit" className="button" value="Submit" onClick={props.onSubmit} />
       </form>
     </>
   );
--- src/pages/receipts/ReceiptNew.tsx
+++ src/pages/receipts/ReceiptNew.tsx
@@ -1,7 +1,14 @@
 import { useMemo, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { PostReceipt, ReceiptItem, ReceiptDate, ReceiptDateTime, UserDetails, ContributorSubtotal } from "../../@types/receipt-manager";
+import {
+  PostReceipt,
+  ReceiptItem,
+  ReceiptDate,
+  ReceiptDateTime,
+  UserDetails,
+  ContributorSubtotal,
+} from "../../@types/receipt-manager";
 import { ReceiptForm } from "../../components/receipt/ReceiptForm";
 import { ItemForm } from "../../components/receipt/ItemForm";
 import { useReceiptContext } from "../../hooks/useReceiptContext";
@@ -9,100 +16,136 @@ import { useReceiptContext } from "../../hooks/useReceiptContext";
 import { Modal } from "../../components/Modal";
 
 interface Props {
-  friends: UserDetails[]
+  friends: UserDetails[];
 }
 
 const ReceiptNew = (props: Props) => {
   const [jwt, setJwt, apiUrl] = useReceiptContext();
-  if(!jwt) {
+  if (!jwt) {
     window.location.replace("/");
   }
 
   const [friends, setFriends] = useState<UserDetails[]>(props.friends);
   const [receiptStore, setReceiptStore] = useState<string>("");
-  const [dateBought, setDateBought] = useState<ReceiptDate>("");
+  const [dateBought, setDateBought] = useState<ReceiptDate>(
+    new Date().toISOString().split("T")[0] as ReceiptDate
+  );
   const [items, setItems] = useState<ReceiptItem[]>([]);
   const [editItem, setEditItem] = useState<ReceiptItem | null>(null);
   const [showItemModal, setShowItemModal] = useState<boolean>(false);
 
+  const [subtotal, contributorSubtotals]: [number, ContributorSubtotal[]] =
+    useMemo(() => {
+      const subtotal = items.reduce(calcSubtotal, 0);
 
-  const [subtotal, contributorSubtotals]: [number, ContributorSubtotal[]] = useMemo(() => {
-    const subtotal = items.reduce(calcSubtotal, 0);
+      const contributorIds = items
+        .map((item) => item.contributorIds)
+        .flat()
+        .filter((value, index, array) => array.indexOf(value) === index);
 
-    const contributorIds = items
-      .map(item => item.contributorIds)
-      .flat()
-      .filter((value, index, array) => array.indexOf(value) === index);
+      const contributors = props.friends.filter((friend) =>
+        contributorIds.includes(friend.id)
+      );
 
-    const contributors = props.friends.filter(friend => contributorIds.includes(friend.id));
+      const contributorSubtotals: ContributorSubtotal[] = contributors.reduce<
+        ContributorSubtotal[]
+      >((acc, curr) => processContributorSubtotals(acc, curr, items), []);
 
-    const contributorSubtotals: ContributorSubtotal[] = contributors.reduce<ContributorSubtotal[]>((acc, curr) => processContributorSubtotals(acc, curr, items), []);
+      console.log(contributorSubtotals);
 
-    console.log(contributorSubtotals);
+      return [subtotal, contributorSubtotals];
+    }, [items]);
 
-    return [subtotal, contributorSubtotals];
-  }, [items]);
 
-  const onAddItemClick = () => {
-    setShowItemModal(true);
-    setEditItem(null);
+  const onSetStore = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setReceiptStore(e.target.value);
   }
 
-  const onAddItem = (contributors:UserDetails[], product: string, rawPrice: string, rawDiscount?: string): void => {
-    const contributorIds = contributors.map(contributor => contributor.id);
+  const onChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDateBought(e.target.value as ReceiptDate);
+  }
+
+  const onAddItemClick = (): void => {
+    setShowItemModal(true);
+    setEditItem(null);
+  };
+
+  const onAddItem = (
+    contributors: UserDetails[],
+    product: string,
+    rawPrice: string,
+    rawDiscount?: string
+  ): void => {
+    const contributorIds = contributors.map((contributor) => contributor.id);
     const price = parseFloat(rawPrice.replace(",", ".").trim());
-    const discount = rawDiscount ? (parseFloat(rawDiscount.replace(",", ".").trim()) || 0.0) : 0.0;
+    const discount = rawDiscount
+      ? parseFloat(rawDiscount.replace(",", ".").trim()) || 0.0
+      : 0.0;
     const newItem: ReceiptItem = {
-      id: items.length == 0 ? 0 : items[items.length-1].id as number + 1,
+      id: items.length == 0 ? 0 : (items[items.length - 1].id as number) + 1,
       product,
       price,
       discount,
-      contributorIds
-    }
-    setItems(prevItems => [...prevItems, newItem]);
+      contributorIds,
+    };
+    setItems((prevItems) => [...prevItems, newItem]);
     setShowItemModal(false);
-  }
+  };
 
-  const onSaveEdit = (id:number, contributors:UserDetails[], product: string, rawPrice: string, rawDiscount?: string): void => {
-    setItems(prevItems => prevItems.map(item => {
-      if(item.id === id) {
-        const contributorIds = contributors.map(contributor => contributor.id);
-        item.product = product;
-        item.contributorIds = contributorIds;
-        item.price = parseFloat(rawPrice.replace(",", ".").trim());
-        item.discount = rawDiscount ? (parseFloat(rawDiscount.replace(",", ".").trim()) || 0.0) : 0.0;
-      }
-      
-      return item
-    }));
+  const onSaveEdit = (
+    id: number,
+    contributors: UserDetails[],
+    product: string,
+    rawPrice: string,
+    rawDiscount?: string
+  ): void => {
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.id === id) {
+          const contributorIds = contributors.map(
+            (contributor) => contributor.id
+          );
+          item.product = product;
+          item.contributorIds = contributorIds;
+          item.price = parseFloat(rawPrice.replace(",", ".").trim());
+          item.discount = rawDiscount
+            ? parseFloat(rawDiscount.replace(",", ".").trim()) || 0.0
+            : 0.0;
+        }
+
+        return item;
+      })
+    );
     setShowItemModal(false);
-    setEditItem(null)
-  }
+    setEditItem(null);
+  };
 
-  const onRemoveItem = (id:number): void => {
-    setItems(prevItems => prevItems.filter(item => item.id as number !== id));
-  }
+  const onRemoveItem = (id: number): void => {
+    setItems((prevItems) =>
+      prevItems.filter((item) => (item.id as number) !== id)
+    );
+  };
 
-  const validateInputs = ():void => {
-    if(receiptStore.length <= 0) {
+  const validateInputs = (): void => {
+    if (receiptStore.length <= 0) {
       throw new Error("No store entered!");
     }
-  
-    if(dateBought.length <= 0) {
+
+    if (dateBought.length <= 0) {
       throw new Error("No date selected!");
     }
-  
-    if(items.length <= 0) {
+
+    if (items.length <= 0) {
       throw new Error("Receipt has no items!");
     }
-  }
+  };
 
   const onSubmitReceipt = (): void => {
     validateInputs();
 
     const receiptItems: ReceiptItem[] = items.map((item) => {
-      const {id: _, ...data} = item;
-      return data
+      const { id: _, ...data } = item;
+      return data;
     });
 
     const receipt: PostReceipt = {
@@ -115,38 +158,44 @@ const ReceiptNew = (props: Props) => {
     fetch(`${apiUrl}/receipts/create`, {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${jwt}`,
-        "Accept": "application/json",
+        Authorization: `Bearer ${jwt}`,
+        Accept: "application/json",
         "Content-Type": "applicaton/json",
       },
-      body: JSON.stringify(receipt)
-    }).then(res => {
-      return res.json();
-    }).then(json => {
-      console.log(json);
-    }).catch(e => {
-      return console.warn(e);
-    });
-  }
+      body: JSON.stringify(receipt),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((e) => {
+        return console.warn(e);
+      });
+  };
 
-  const hasEnteredData = receiptStore.length > 0 || dateBought.length > 0;
+  const displayStore = receiptStore || "[Store]";
 
   return (
     <>
       <h1 className="text-3xl">Receipt creator!</h1>
       <div className="flex justify-between">
         <div className="w-1/2 mr-10">
-          <ReceiptForm receiptStore={receiptStore} setReceiptStore={setReceiptStore} dateBought={dateBought} setDateBought={setDateBought} />
-          <button className="button" onClick={onAddItemClick}>Add item</button>
-          <input type="submit" name="submit" className="button" value="Submit" onClick={onSubmitReceipt} />
+          <ReceiptForm
+            receiptStore={receiptStore}
+            onSetStore={onSetStore}
+            dateBought={dateBought}
+            onChangeDate={onChangeDate}
+            onAddItem={onAddItemClick}
+            onSubmit={onSubmitReceipt}
+          />
         </div>
         <div className="w-full ml-40 border-2 text-center">
-          {hasEnteredData &&
-            <header className="mt-1 pb-2 border-b-2 border-dashed">
-              <p className="text-3xl font-bold">{receiptStore}</p>
-              <p>{dateBought}</p>
-            </header>
-          }
+          <header className="mt-1 pb-2 border-b-2 border-dashed">
+            <p className="text-3xl font-bold">{displayStore}</p>
+            <p>{dateBought}</p>
+          </header>
           <div className="receipt-body flex flex-col grow justify-between h-3/5">
             <div className="mb-2">
               <div className="flex justify-between mx-3">
@@ -158,62 +207,88 @@ const ReceiptNew = (props: Props) => {
                   <p className="font-bold">DISCOUNT</p>
                 </div>
               </div>
-              {items.map(item => {
-                return(
-                <div key={item.product} className="flex justify-between mx-3">
-                  <div className="flex justify-between items-center max-w-1/10 w-full align-middle">
-                    <p className="mr-2">{item.product}</p>
-                    <FontAwesomeIcon className="text-sm hover:cursor-pointer text-blue-300" icon="pen" onClick={() => {
-                        setEditItem(item);
-                        setShowItemModal(true);
-                      }} />
-                    <FontAwesomeIcon className="text-sm hover:cursor-pointer text-red-300" icon="xmark" onClick={() => onRemoveItem(item.id as number)} />
+              {items.map((item) => {
+                return (
+                  <div key={item.product} className="flex justify-between mx-3">
+                    <div className="flex justify-between items-center max-w-1/10 w-full align-middle">
+                      <p className="mr-2">{item.product}</p>
+                      <FontAwesomeIcon
+                        className="text-sm hover:cursor-pointer text-blue-300"
+                        icon="pen"
+                        onClick={() => {
+                          setEditItem(item);
+                          setShowItemModal(true);
+                        }}
+                      />
+                      <FontAwesomeIcon
+                        className="text-sm hover:cursor-pointer text-red-300"
+                        icon="xmark"
+                        onClick={() => onRemoveItem(item.id as number)}
+                      />
+                    </div>
+                    <div className="flex justify-between w-2/12">
+                      <p className="">{item.price}</p>
+                      <p>{item.discount}</p>
+                    </div>
                   </div>
-                  <div className="flex justify-between w-2/12">
-                    <p className="">{item.price}</p>
-                    <p>{item.discount}</p>
-                  </div>
-                </div>
                 );
               })}
             </div>
             <div className="text-left border-t-2 border-solid p-1 pl-2">
-              {contributorSubtotals.length > 0 && contributorSubtotals.map(contributorSubtotal => {
-                return(
-                  <p key={contributorSubtotal.contributor.id}>{contributorSubtotal.contributor.firstName}'s share: {contributorSubtotal.subtotal}</p>
-                  )
-                })
-              }
+              {contributorSubtotals.length > 0 &&
+                contributorSubtotals.map((contributorSubtotal) => {
+                  return (
+                    <p key={contributorSubtotal.contributor.id}>
+                      {contributorSubtotal.contributor.firstName}'s share:{" "}
+                      {contributorSubtotal.subtotal}
+                    </p>
+                  );
+                })}
               <p className="mt-2">Subtotal: {subtotal}</p>
             </div>
           </div>
         </div>
       </div>
-      {showItemModal &&
-      <Modal title="Add item to receipt" show={showItemModal} setShow={setShowItemModal}>
-        <ItemForm friends={friends} addItem={onAddItem} onSave={onSaveEdit} editItem={editItem} />
-      </Modal>
-      }
+      {showItemModal && (
+        <Modal
+          title="Add item to receipt"
+          show={showItemModal}
+          setShow={setShowItemModal}
+        >
+          <ItemForm
+            friends={friends}
+            addItem={onAddItem}
+            onSave={onSaveEdit}
+            editItem={editItem}
+          />
+        </Modal>
+      )}
     </>
-  )
-}
+  );
+};
 
 const calcSubtotal = (subtotal: number, item: ReceiptItem) => {
-  subtotal += (item.price - item.discount)
+  subtotal += item.price - item.discount;
   return subtotal;
-}
+};
 
 const calcContributorTotal = (totalContribution: number, item: ReceiptItem) => {
-  if(!item.contributorIds) {
+  if (!item.contributorIds) {
     return totalContribution;
   }
 
   totalContribution += (item.price - item.discount) / item.contributorIds.length;
   return totalContribution;
-}
+};
 
-const processContributorSubtotals = (subtotals: ContributorSubtotal[], contributor:UserDetails, items: ReceiptItem[]) => {
-  const contributorItems = items.filter(item => item.contributorIds?.includes(contributor.id));
+const processContributorSubtotals = (
+  subtotals: ContributorSubtotal[],
+  contributor: UserDetails,
+  items: ReceiptItem[]
+) => {
+  const contributorItems = items.filter((item) =>
+    item.contributorIds?.includes(contributor.id)
+  );
 
   const total = contributorItems.reduce(calcContributorTotal, 0);
 
@@ -222,6 +297,6 @@ const processContributorSubtotals = (subtotals: ContributorSubtotal[], contribut
     subtotal: total,
   });
   return subtotals;
-}
+};
 
 export default ReceiptNew;
